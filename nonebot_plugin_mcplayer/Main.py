4916c30f164c8263edead8169101b01f04ddbb5a
Sky_Dynamic <82658163+SkyDynamic@users.noreply.github.com>
2022-08-16 17:17:29 +0800

    1.2.1版本 修复无法删除绑定服务器的Bug 新增修改服务器信息功能

--- nonebot_plugin_mcplayer/Main.py
+++ nonebot_plugin_mcplayer/Main.py
@@ -23,4 +23,8 @@ class Main():
                 message = f'------{server_name}当前状态------\n' + f'最大人数：{str(status.players.max)}\n' + f'当前在线人数：{str(status.players.online)}\n' + '当前在线玩家：\n- ' + '\n- '.join(player_list)
             except TypeError as e:
                 message = f'------{server_name}当前状态------\n' + f'最大人数：{str(status.players.max)}\n' + f'当前在线人数：0\n' + '当前在线玩家：\n\n--没有玩家在线--'
+            except TimeoutError as e2:
+                message = '[TimeOutError]服务器连接超时，请稍后再试。如仍然无法连接请检查服务器是否能正常连接并联系机器人管理员'
+            except OSError as e1:
+                message = '[OSError]服务器信息获取失败，请检查您的服务器是否开启或能否连接'
             return message
